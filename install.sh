64e04fa4e5b400a495c07240d0eb005160b85c58
Sythnog <Sythnog@protonmail.com>
2020-10-25 13:17:13 +0100

    Install script now works with pacman and apt

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 echo -e "\n-==INSTALL FOR ROBO RED TEAM TRAINING PLATFORM==-\n"
-# Python dependencies
-pip install flask flask_restful docker
-# Docker dependencies
+
+packages="python python-pip docker"
+pythonDependencies="flask flask_restful docker" 
+
+# Installation of needed packages
+if [ "$1" == "pacman" ]; then
+    pacman -S $packages
+    pip install $pythonDependencies
+elif [ "$1" == "apt" ]; then
+    apt install $packages
+    pip install $pythonDependencies
+else
+    echo -e "Please manually install: $packages\nand these Python libaries: $pythonDependencies\nOr run the script with 'pacman' or 'apt' as param."
+fi
+
+# Docker build
 echo -e "\nPlase manually build the desired docker images\nYou can find them in ./machines/docker/*\n"
-# OS dependencies 
-echo -e "\nPlease make sure your system has: python, python-pip, docker\n"

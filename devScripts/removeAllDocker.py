48d805b687db71d80c8d36ebd0d4819c59a7032d
Sythnog <Sythnog@protonmail.com>
2020-11-05 10:36:15 +0100

    Added simple Vagrantfile

--- devScripts/removeAllDocker.py
+++ devScripts/removeAllDocker.py
@@ -7,8 +7,6 @@ if len(sys.argv) > 1 and sys.argv[1] == "yes":
     removeCmd = ""
     for container in client.containers.list():
         removeCmd += "$(bash removeDockerContainer.sh " + str(container.id[0:10]) + ") & "
-    print(removeCmd + "wait && docker system prune -a")
     os.system(removeCmd + "wait && docker system prune -a && docker ps")
 else:
     print("Please run script with argument 'yes' to confirm you wish to remove containers and services")
-

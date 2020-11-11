70606b7542f2c2fb2b905a4461f5e452c821a0c2
Sythnog <Sythnog@protonmail.com>
2020-11-11 19:00:36 +0100

    Updated install script for apt systems

--- install.sh
+++ install.sh
@@ -2,27 +2,28 @@
 echo -e "\n-==INSTALL FOR ROBO RED TEAM TRAINING PLATFORM==-\n"
 
 # Dependencies
-packages="python python-pip docker vagrant"
+packages="python vagrant"
 pythonDependencies="flask flask_restful flask_cors docker python-vagrant"
 systemctlStart="docker" 
 
 # Common things for pacman- and apt based systems
 CommonSetup() {
-    pip install $pythonDependencies
     systemctl enable $systemctlStart
     systemctl start $systemctlStart
 }
 
 # Installation of needed packages
 if [ "$1" == "pacman" ]; then
-    pacman -S $packages
+    pacman -S $packages docker python-pip
+    pip install $pythonDependencies
     CommonSetup
 elif [ "$1" == "apt" ]; then
     apt update
-    apt install $packages
+    apt install $packages docker.io python3-pip
+    pip3 install $pythonDependencies
     CommonSetup
 else
-    echo -e "Please manually install: $packages\nand these Python libaries: $pythonDependencies\nOr run the script with 'pacman' or 'apt' as param."
+    echo -e "Please manually install: $packages [docker/docker.io] [pip for python3]\nand these Python libaries: $pythonDependencies\nOr run the script with 'pacman' or 'apt' as param."
 fi
 
 # Docker build

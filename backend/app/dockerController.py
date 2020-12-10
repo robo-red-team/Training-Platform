275c972d820a5e2ec02ae2076fd980fc62f68f38
0xlimE <58474540+0xlimE@users.noreply.github.com>
2020-12-10 18:27:12 +0100

    Update dockerController.py
    Need to spawn containers with network

--- backend/app/dockerController.py
+++ backend/app/dockerController.py
@@ -11,12 +11,12 @@ def get_random_string(length):
 
 # Spawn container, and return it's ID
 def SpawnContainer(ContainerName):
-    container = client.containers.run(ContainerName, detach=True)
+    container = client.containers.run(ContainerName, detach=True, network="docker-vpn")
     return str(container.id[0:10])
 
 def SpawnContainerWithPass(ContainerName):
     passwd  = get_random_string(8)
-    container = client.containers.run(ContainerName, passwd, detach=True)
+    container = client.containers.run(ContainerName, passwd, detach=True, network="docker-vpn")
     return {"id":str(container.id[0:10]),"password":passwd}
 
 def GetAllContainers():

